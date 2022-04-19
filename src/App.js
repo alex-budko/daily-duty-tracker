28b0c47af47cd23bc8b6b051cc88c337f4a0ae80
alex-budko <xalex.budko@gmail.com>
2022-04-19 15:26:42 -0400

    added the ability to set reminder to tasks

--- src/App.js
+++ src/App.js
@@ -25,11 +25,15 @@ const App = () => {
     setTasks(tasks.filter((task)=> task.id !== id))
   }
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task)=> task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
 
   return (
     <div className="container">
       <Header title = "Daily Duty Tracker" />
-      {tasks.length > 0 ? <Tasks tasks = {tasks} onDelete={deleteTask} />
+      {tasks.length > 0 ? <Tasks tasks = {tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
       : 'No Duties To Show'}
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,11 @@
 import { FaTimes } from 'react-icons/fa'
 
-const Task = ({task, onDelete}) => {
+const Task = ({task, onDelete, onToggle}) => {
   return (
-    <div className="task">
-        <h3>{task.text} <FaTimes style={{color: 'red', cursor: 'pointer'}} onClick={()=>onDelete(task.id)} />
+    //if task is set to reminder = true, then additional class is added of 'reminder' 
+    <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={()=>onToggle(task.id)}>
+        <h3>   {task.text} 
+        <FaTimes style={{color: 'red', cursor: 'pointer'}} onClick={()=>onDelete(task.id)} />
         </h3>
         <p>{task.day}</p>
     </div>
