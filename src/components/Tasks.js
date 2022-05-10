0e130864892120d19aa202a32caa1f80585d6482
alex-budko <xalex.budko@gmail.com>
2022-05-09 22:48:14 -0400

    added tasks comments

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -2,6 +2,7 @@ import Task from './Task'
 
 const Tasks = ({tasks, onDelete, onToggle}) => {
     return (
+        //maps through tasks & sets them
         <>
             {tasks.map((task, index) => (
                 <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
